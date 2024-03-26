82a68f9e0767dde37a371dd4485a772024b4b14d
KumailIqbal <kumailiqbal97@gmail.com>
2024-03-27 02:14:29 +0530

    commit 3

---
+++ App.js
@@ -0,0 +1,3 @@
+const heading = React.createElement("h1", {id: "head", xyz: "abc"}, "Hello World using React!");
+    const root = ReactDOM.createRoot(document.getElementById("root"));
+    root.render(heading);
