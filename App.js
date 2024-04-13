ac12743d0888d33c2ab28cdbdf23a30f2293ecc5
KumailIqbal <kumailiqbal97@gmail.com>
2024-04-14 01:27:29 +0530

    new commit

--- App.js
+++ App.js
@@ -1,6 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
 const parent = React.createElement("div", {}, 
 React.createElement("div", {id: "child1"}, [
-React.createElement("h1", {},"I am an h1 tag") ,
+React.createElement("h1", {},"I am learning react") ,
 React.createElement("h2", {},"I am an h2 tag")
 ]),
 React.createElement("div", {id: "child2"}, [
